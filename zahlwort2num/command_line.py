e5693d7eea2bdb975eb9a5e935acc7b84f695463
Piotr Walkowski <piotr@deluxe-soft.com>
2026-01-22 11:31:51 +0100

    Release 0.4.3

--- zahlwort2num/command_line.py
+++ zahlwort2num/command_line.py
@@ -5,7 +5,11 @@ import sys
 
 
 def main():
+    # Debug: print what we're getting
+    print(f"sys.argv: {sys.argv}, len: {len(sys.argv)}")
     if (len(sys.argv) > 1) and sys.argv[1]:
+        print(f"Trying to convert: {sys.argv[1]}")
         print(w2n.convert(sys.argv[1]))
     else:
+        print("No parameter given, raising ArgumentError")
         raise ArgumentError('No parameter given!')
